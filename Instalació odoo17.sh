4e7822a6cac6d56dfd8d2c10408491560b461c1a
Oscar <151764545+OMCbusiness@users.noreply.github.com>
2025-01-15 10:58:04 +0100

    Update Instalació odoo17.sh

--- Instalació odoo17.sh
+++ Instalació odoo17.sh
@@ -19,40 +19,48 @@ sudo dnf install -y certbot nginx
 sudo mkdir -p /etc/nginx/sites-available && sudo mkdir -p /etc/nginx/sites-enabled
 
 # Fichero nginx.conf
-sudo rm -r /etc/nginx/nginx.conf
 
+# Eliminar el archivo de configuración de nginx si existe
+sudo rm -f /etc/nginx/nginx.conf
+
+# Crear el nuevo archivo de configuración
 sudo sh -c 'cat <<EOF > /etc/nginx/nginx.conf
-#For more information on configuration, see:
+# For more information on configuration, see:
 # * Official English Documentation: http://nginx.org/en/docs/
 # * Official Russian Documentation: http://nginx.org/ru/docs/
 user nginx;
 worker_processes auto;
 error_log /var/log/nginx/error.log;
 pid /run/nginx.pid;
+
 # Load dynamic modules. See /usr/share/doc/nginx/README.dynamic.
 include /usr/share/nginx/modules/*.conf;
+
 events {
     worker_connections 1024;
 }
+
 http {
-   log_format main '$remote_addr - $remote_user [$time_local] "$request" '
-      '$status $body_bytes_sent "$http_referer" '
-      '"$http_user_agent" "$http_x_forwarded_for"';
-   access_log /var/log/nginx/access.log main;
-   sendfile on;
-   tcp_nopush on;
-   tcp_nodelay on;
-   keepalive_timeout 65;
-   types_hash_max_size 2048;
-   include /etc/nginx/mime.types;
-   default_type application/octet-stream;
-   # Load modular configuration files from the /etc/nginx/conf.d directory.
-   # See http://nginx.org/en/docs/ngx_core_module.html#include
-   # for more information.
-   include /etc/nginx/conf.d/*.conf;
-   include /etc/nginx/sites-enabled/*.conf;
+    log_format main '\$remote_addr - \$remote_user [\$time_local] "\$request" '
+        '\$status \$body_bytes_sent "\$http_referer" '
+        '"\$http_user_agent" "\$http_x_forwarded_for"';
+    access_log /var/log/nginx/access.log main;
+    sendfile on;
+    tcp_nopush on;
+    tcp_nodelay on;
+    keepalive_timeout 65;
+    types_hash_max_size 2048;
+    include /etc/nginx/mime.types;
+    default_type application/octet-stream;
+
+    # Load modular configuration files from the /etc/nginx/conf.d directory.
+    # See http://nginx.org/en/docs/ngx_core_module.html#include
+    # for more information.
+    include /etc/nginx/conf.d/*.conf;
+    include /etc/nginx/sites-enabled/*.conf;
 }
 EOF'
+
 
 # Crear el archivo dhparam.pem
 sudo mkdir -p /etc/cert/certs
