caaa524aab7059448f9ea61f20021dcf23b26454
Oscar <151764545+OMCbusiness@users.noreply.github.com>
2025-01-20 18:58:48 +0100

    Update Instalació odoo17.sh

--- Instalació odoo17.sh
+++ Instalació odoo17.sh
@@ -260,3 +260,6 @@ sudo firewall-cmd --add-port=80/tcp && sudo firewall-cmd --add-port=443/tcp && s
 
 # Guardar la configuración del firewalld
 sudo firewall-cmd --runtime-to-permanent
+#Permisos
+sudo chown -R odoo /opt/odoo
+sudo chgrp -R odoo /opt/odoo
